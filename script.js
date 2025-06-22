5898b54a741289db887150c40b3be7fbea33322f
Giovana Marques Silva <giovanamarquessilva24@gmail.com>
2025-06-22 19:51:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+//video
+document.addEventListener("DOMContentLoaded", function () {
+    let openVideoBtn = document.getElementById("open-video-btn");
+    let videoModal = document.getElementById("video-modal");
+    let videoContainer = document.getElementById("video-container");
+    let videoPlayer = document.getElementById("video-player");
+    let closeBtn = document.getElementById("close-btn");
+
+    openVideoBtn.addEventListener("click", function (event) {
+        event.preventDefault(); 
+
+       
+        videoModal.style.display = "flex";
+        videoPlayer.play(); 
+    });
+
+    
+    closeBtn.addEventListener("click", function () {
+        videoModal.style.display = "none";
+        videoPlayer.pause(); 
+        videoPlayer.currentTime = 0; 
+    });
+
+    window.addEventListener("click", function (event) {
+        if (event.target === videoModal) {
+            videoModal.style.display = "none";
+            videoPlayer.pause();
+            videoPlayer.currentTime = 0;
+        }
+    });
+});
+
+//digitação
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementById("typewriter");
+
+  const text = `Welcome to my portfolio! I'm Giovana Marques Silva,
+  passionate about technology and innovation. Here you will find
+  my projects, experiences and learnings in the development area. 
+  Explore and find out more about my work!`;
+
+  let index = 0;
+
+  function type() {
+    const current = text.slice(0, index);
+    const cursor = `<span class="cursor">|</span>`;
+    element.innerHTML = current.replace(/\n/g, "<br>") + cursor;
+
+    if (index < text.length) {
+      index++;
+      setTimeout(type, 30); 
+    }
+  }
+
+  type();
+});
+
+
+
+// Inicia a animação 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('ativo');
+    } else {
+      entry.target.classList.remove('ativo');
+    }
+  });
+}, { threshold: 0.3 });
+
+const sectionsToObserve = [
+  document.getElementById('technology'),
+  document.getElementById('project'),
+  document.getElementById('about-me')
+];
+
+sectionsToObserve.forEach(section => {
+  if(section) observer.observe(section);
+});
+
